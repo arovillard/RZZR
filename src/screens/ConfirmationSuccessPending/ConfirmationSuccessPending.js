0e9603868f6e06fa5b3d503c7365db57affa8e47
Nitika <nitika.jindal01@loblaw.ca>
2022-11-04 14:01:05 -0400

    fixed the color

--- src/screens/ConfirmationSuccessPending/ConfirmationSuccessPending.js
+++ src/screens/ConfirmationSuccessPending/ConfirmationSuccessPending.js
@@ -9,7 +9,7 @@ import { success } from '@/assets';
 export function ConfirmationSuccessPending() {
   const { colors } = useTheme();
   return (
-    <View style={[styles.container, { backgroundColor: 'white' }]}>
+    <View style={[styles.container, { backgroundColor: colors.white }]}>
       <Image accessibilityIgnoresInvertColors source={success} style={{ width: 70, height: 70 }} />
       <Text style={[typography.text, { textAlign: 'center' }]}>
         Your usage Ticket has been successfully submitted.
